ed236b79611ca3e598370c08c4af94c55ba4ee2b
Parth795142 <kparth922@gmail.com>
2024-11-01 22:18:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
              
              let page;
              
-             let email = 'yaminiravila17@gmail.com'
+             let email =Chhaya_bramnhotra_
              
              let password = 'Yamini@1717'
              
